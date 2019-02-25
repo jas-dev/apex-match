b4385fe2172214bd3c8ab9c722efc06d324597af
jas-dev <jaysamora@outlook.com>
2019-02-24 22:10:46 -0800

    v0.5 and v1.0 finished

--- main.js
+++ main.js
@@ -4,31 +4,29 @@ $(document).ready(startGame);
 
 var firstCard = null;
 var secondCard = null;
-var currentMatches = null;
-var gamesPlayed = null;
-//counter that increments when reset button clicked, .text value to "games-played > value
-var attempts = null;
-//counter for number of times firstCard and secondCard pair were clicked. resets when reset button clicked. .text to "attempts > value
-var accuracy = null;
-//divide totalMatches by totalAttempts, resets when reset is clicked
+var matches = 0;
+var gamesPlayed = 0;
+var attempts = 0;
+var accuracy = 0 + "%";
 var canBeClicked = true;
 
-//reset function to wipe stats needed
+
 
 
 function startGame(){
     console.log('Game started');
     addEventHandlers();
-
+    display_stats();
 }
 
 function addEventHandlers(){
     $('.card').click(clickHandler);
-
+    $('.reset').click(reset_stats);
 }
 
 function clickHandler(){
-    if(!canBeClicked ){ // || no clicky same card cheater//
+    if(!canBeClicked || $(this).hasClass('revealed')){
+        //if caBeClicked var is falsy exit click handler so game "stops playing" until its truthy again, or if card that was clicked already has class of revealed, exit click handler for that moment so multiple clicks on same element cant be used to exploit game logic
         return;
     }
     $(this).addClass('revealed');
@@ -43,17 +41,19 @@ function clickHandler(){
         var secondCardImg = $(secondCard).find('.front').css('background-image');
         //finds front background image of the second card clicked, returns its value and stores in firstCardImg
         if(firstCardImg === secondCardImg){
-            currentMatches++;
+            matches++;
             attempts++;
             console.log ('MATCH');
             firstCard = null;
             secondCard = null;
+            display_stats();
 
         }else{
             attempts++;
             console.log('NO MATCH');
             canBeClicked = false;
-            setTimeout(resetCards, 2000)
+            setTimeout(resetCards, 1500);
+            display_stats();
         }
 
     }
@@ -62,10 +62,40 @@ function clickHandler(){
 }
 
 function resetCards(){
-    console.log(firstCard);
+    console.log("resetCards has been triggered");
     $(firstCard).removeClass('revealed');
     $(secondCard).removeClass('revealed');
     firstCard = null;
     secondCard = null;
     canBeClicked = true;
 }
+
+function display_stats(){
+    console.log("display_stats has been triggered");
+    $('.games-played .value').text(gamesPlayed);
+    $('.attempts .value').text(attempts);
+    if(!matches && !attempts){
+        accuracy = null;
+        $('.accuracy .value').text(accuracy);
+    }else{
+        accuracy = matches / attempts;
+        var accuracyConverted= (accuracy*100).toFixed(2)+ "%";
+        $('.accuracy .value').text(accuracyConverted);
+    }
+
+
+
+}
+
+function reset_stats(){
+    console.log("reset stats has been triggered");
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    firstCard = null;
+    secondCard = null;
+    $('div .card').removeClass('revealed');
+    gamesPlayed++;
+    display_stats();
+}
+
